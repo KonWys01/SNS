0947d0101a3505d66f9498b16c6b4b7bc81e7879
KonWys01 <konradwysokinski01@gmail.com>
2022-03-13 10:36:47 +0100

    interval, date_to_list, algorythm(x,y,z)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-
 import numpy as np
 
 from python.read_yuma import read_yuma
@@ -12,6 +11,7 @@ class Satellites:
         self.naval = self.read_file()
         self.start_date = datetime(year=2022, month=2, day=25)
         self.end_date = datetime(year=2022, month=2, day=26)
+        self.interval = timedelta(minutes=15)
 
     def read_file(self):
         return read_yuma(self.file)
@@ -19,6 +19,53 @@ class Satellites:
     def set_start_end_dates(self, start_date: datetime, end_date: datetime):
         self.start_date = start_date
         self.end_date = end_date
+
+    @staticmethod
+    def datetime_to_list(date: datetime):
+        date = [date.year,
+                date.month,
+                date.day,
+                date.hour,
+                date.minute,
+                date.second]
+        return date
+
+    def satellite_xyz(self, week: int, tow: int):
+        id, health, e, toa, i, omega_dot, sqrta, Omega, omega, m0, alfa, alfa1, gps_week = self.naval
+
+        t = week * 7 * 86400 + tow
+        toa_weeks = gps_week * 7 * 86400 + toa
+        tk = t - toa_weeks
+        # print(tk)
+
+        """algorytm"""
+        u = 3.986005 * (10 ** 14)
+        omega_e = 7.2921151467 * (10 ** -5)
+        # tk = t - toa
+        a = sqrta ** 2
+        n = np.sqrt(u / (a ** 3))
+        Mk = m0 + n * tk
+
+        E1 = Mk
+        Ei = Mk + e * np.sin(E1)
+        while np.fabs(Ei - E1) >= (10 ** -12):
+            E1 = Ei
+            Ei = Mk + e * np.sin(E1)
+
+        Ek = Ei
+        vk = np.arctan2(np.sqrt(1 - e ** 2) * np.sin(Ek), np.cos(Ek) - e)
+
+        phi_k = vk + omega
+        rk = a * (1 - e * np.cos(Ek))
+        xk = rk * np.cos(phi_k)
+        yk = rk * np.sin(phi_k)
+        omega_k = Omega + (omega_dot - omega_e) * tk - omega_e * toa
+
+        Xk = xk * np.cos(omega_k) - yk * np.cos(i) * np.sin(omega_k)
+        Yk = xk * np.sin(omega_k) + yk * np.cos(i) * np.cos(omega_k)
+        Zk = yk * np.sin(i)
+
+        return Xk, Yk, Zk
 
 
 if __name__ == "__main__":
